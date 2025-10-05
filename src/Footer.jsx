8fedc5afa6a6f65074484230587d38bd08498723
Sara Abraham <sara.abraham12@gmail.com>
2025-10-05 17:27:05 +0200

    Fixed images

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,15 @@
+import insta from "./assets/Instagram Icon.png"
+import git from "./assets/GitHub Icon.png"
+
 export default function Footer() {
     return (
         <div className="footer">
             <a href="https://www.instagram.com/sara._.abraham_?utm_source=qr&igsh=cjZmcGFibWRwZHdu" target="_blank" className="icon">
-                <img src="./src/assets/Instagram Icon.png" className="insta-icon"></img>
+                <img src={insta} className="insta-icon"></img>
             </a>
 
             <a href="https://github.com/saraabraham" target="_blank" className="icon">
-                <img src="./src/assets/GitHub Icon.png" className="git-icon"></img>
+                <img src={git} className="git-icon"></img>
             </a>
 
 
