cc4f8cc366b3ff710471f4c45cac85407411405e
Sara Abraham <sara.abraham12@gmail.com>
2025-10-05 17:10:13 +0200

    first Draft

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+            <a href="https://www.instagram.com/sara._.abraham_?utm_source=qr&igsh=cjZmcGFibWRwZHdu" target="_blank" className="icon">
+                <img src="src/assets/Instagram Icon.png" className="insta-icon"></img>
+            </a>
+
+            <a href="https://github.com/saraabraham" target="_blank" className="icon">
+                <img src="src/assets/GitHub Icon.png" className="git-icon"></img>
+            </a>
+
+
+        </div>
+
+    )
+
+
+}
---
+++ src/Interests.jsx
@@ -0,0 +1,10 @@
+
+export default function Interests() {
+    return (
+        <div className="interests">
+            <h1>Interests</h1>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Bollywood ninja. Coffee fanatic.</p>
+
+        </div>
+    )
+}
