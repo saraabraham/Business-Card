0fe973e80850d3a65f8199faf0265ff5f94e7228
Sara Abraham <sara.abraham12@gmail.com>
2025-10-06 16:53:39 +0200

    Added resume

--- src/Interests.jsx
+++ src/Interests.jsx
@@ -1,10 +1,13 @@
-
+// import pdf from "./public/Sara_Abraham_6:oct.pdf"
 export default function Interests() {
     return (
         <div className="interests">
             <h1>Interests</h1>
             <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Bollywood ninja. Coffee fanatic.</p>
+            <br />
+            < a className="Resume" href="/Sara_Abraham_6:oct.pdf" target="_blank">Click here to download resume!</a>
 
         </div>
+
     )
 }
